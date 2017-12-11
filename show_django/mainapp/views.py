b77bbd96fdc09c499ddc24b0e749175333f25d6f
mrm <mrm@tradebyte.biz>
2017-12-11 17:02:52 +0100

    first templates

--- show_django/mainapp/views.py
+++ show_django/mainapp/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse    #add 
 
-# Create your views here.
-def index(request):                                     #add
-    return HttpResponse('<h1>Hello World!</h1>')        #add
+def index(request):
+    return render(request, 'index.html')
 
-def marmet(request):
-    return HttpResponse('<h1>funzzt</h1>')        #add
-    
--- show_django/show_django/settings.py
+++ show_django/show_django/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mainapp', 
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
