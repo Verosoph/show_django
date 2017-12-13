355309a44b3e77f9ed595b72e3efe2fc64c7bd23
Niklas Heer <niklas.heer@gmail.com>
2017-12-13 16:56:07 +0100

    model-handling

--- show_django/mainapp/models.py
+++ show_django/mainapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Data(models.Model):                           #add
+    name = models.CharField(max_length=100)         #add
+    value = models.CharField(max_length=100)        #add
+    location = models.CharField(max_length=20)      #add
+    
+    def __str__(self):                              #add
+        return self.name                            #add
